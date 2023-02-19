14e92ecd4a903353b17548ed346a249fe66b0670
nourbaatour <nour.baatour@esprit.tn>
2023-02-19 23:25:44 +0100

    mod user

--- src/Controller/UserAccessController.php
+++ src/Controller/UserAccessController.php
@@ -1,69 +1,78 @@
 <?php
-
 namespace App\Controller;
-
+    
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\EntityManagerInterface;
-
-class UserAccessController extends AbstractController
-{
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->entityManager = $entityManager;
-    }
-
-    /**
-     * @Route("/access", name="display")
-     */
-    public function index(): Response
-    {
-        $users = $this->entityManager
-            ->getRepository(User::class)
-            ->findAll();
-    
-        return $this->render('admin/dashboard.html.twig', [
-            'users' => $users
-        ]);
-    }
-    
- /**
- * @Route("/users/{id}/edit", name="user_edit")
- */
-public function edit(Request $request, User $user): Response
-{
-    $form = $this->createForm(RegistrationFormType::class, $user);
-    $form->handleRequest($request);
-
-    if ($form->isSubmitted() && $form->isValid()) {
-        // Get the roles as a comma-separated string
-        $roles = implode(',', $user->getRoles());
+ use Doctrine\ORM\EntityManagerInterface;
         
-        // Set the roles of the user entity
-        $user->setRoles([$roles]);
+        class UserAccessController extends AbstractController
+        {
+            private $entityManager;
+        
+            public function __construct(EntityManagerInterface $entityManager)
+            {
+                $this->entityManager = $entityManager;
+            }
+        
+            /**
+             * @Route("/access", name="display")
+             */
+            public function index(): Response
+            {
+                $users = $this->entityManager
+                    ->getRepository(User::class)
+                    ->findAll();
+            
+                return $this->render('admin/dashboard.html.twig', [
+                    'users' => $users
+                ]);
+            }
+            
+            /**
+             * @Route("/users/{id}/edit", name="user_edit")
+             */
+            public function edit(Request $request, User $user): Response
+            {
+                $form = $this->createForm(RegistrationFormType::class, $user);
+                $form->handleRequest($request);
+        
+                if ($form->isSubmitted() && $form->isValid()) {
+                    // Get the roles as a comma-separated string
+                    $roles = implode(',', $user->getRoles());
+                
+                    // Set the roles of the user entity
+                    $user->setRoles([$roles]); // Handle the uploaded file
+                    $file = $form->get('Photo')->getData();
+                    if ($file) {
+                        $fileName = md5(uniqid()) . '.' . $file->guessExtension();
+                        $file->move(
+                            $this->getParameter('img_directory'),
+                            $fileName
+                        );
+                        $user->setImage($fileName);
+                    }
 
-        $this->entityManager->flush();
-
-        // Create a new form with the updated data
-        $newForm = $this->createForm(RegistrationFormType::class, $user);
-
-        // Render the new form
-        return $this->render('admin/edit.html.twig', [
-            'form' => $newForm->createView(),
-        ]);
-    }
-
-    return $this->render('admin/edit.html.twig', [
-        'user' => $user,
-        'form' => $form->createView(),
-    ]);
-}
+        
+                    $this->entityManager->flush();
+        
+                    // Create a new form with the updated data
+                    $newForm = $this->createForm(RegistrationFormType::class, $user);
+        
+                    // Render the new form
+                    return $this->render('admin/edit.html.twig', [
+                        'form' => $newForm->createView(),
+                    ]);
+                }
+        
+                return $this->render('admin/edit.html.twig', [
+                    'user' => $user,
+                    'form' => $form->createView(),
+                ]);
+            }
 
     /* kbal manzid fzet el role
     public function edit(Request $request, User $user): Response
